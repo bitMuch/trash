c60f67c3d63e4adf155385a35003c24a3154ea9a
bitMuch <bitmuch@protonmail.com>
2017-03-19 22:29:19 +0000

    [+] guess_dice/roll.c

--- guess_dice/roll.c
+++ guess_dice/roll.c
@@ -5,18 +5,23 @@
 int main () {
 	
 	while (1) {
-	  int guess;
+		char buffer[2];
     printf("What number will I roll?: ");
-		scanf("%d", &guess);
+		fgets(buffer,2,stdin);
+    int guess;
+    guess = atoi(buffer);
+    
 	  int roll;
 	  roll = dice();
 
-    if(guess==roll) {
+    if (guess==roll) {
 		  printf("Hey, you guessed right!\r\n");
 	  }
 
 		else {
 		  printf("Guess again\r\n");
 	  }
-  }
+    int c;
+		while ( (c = getchar()) != '\n' && c != EOF ) { }
+	}
 }
